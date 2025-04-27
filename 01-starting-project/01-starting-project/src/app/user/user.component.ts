8a58f40122eb989a937432322a92a8973332c2e4
Roi Tanimoto <roi.tanimoto@gmail.com>
2025-04-27 03:08:44 -1000

    Input signals

---
+++ 01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, computed, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectedUser() {}
+}
