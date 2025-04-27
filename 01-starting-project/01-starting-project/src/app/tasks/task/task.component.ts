7943bd88ed20da8e84eb9111033fc4e5c55fca56
Roi Tanimoto <roi.tanimoto@gmail.com>
2025-04-27 08:12:02 -1000

    added new task service

--- 01-starting-project/01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,19 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  constructor(private tasksServce: TasksService) {}
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksServce.removeTask(this.task.id);
   }
 }
--- 01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -8,12 +8,14 @@ import {
   output,
 } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [CardComponent],
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
