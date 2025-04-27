6edabad36ec86d7c81bb0a94545d772c0a686eec
Roi Tanimoto <roi.tanimoto@gmail.com>
2025-04-27 03:44:57 -1000

    Output for emitting signals

--- 01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,12 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  input,
+  computed,
+  output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,17 +15,38 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+
+  // using input signals
+
   // avatar = input.required<string>();
   // name = input.required<string>();
+
+  // using signals
+
+  @Output() select = new EventEmitter();
+
+  // using output
+
+  // select = output<string>();
+
+  // using output
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
+
+  // using input signals
+
   // imagePath = computed(() => {
   //   return 'assets/users/' + this.avatar();
   // });
 
-  onSelectedUser() {}
+  // using signals
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
